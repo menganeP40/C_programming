0b8414ca494bfa92d39fcf2c5d4324524f2dc0fa
menganeP40 <menganeprashant40@gmail.com>
2024-10-18 20:32:23 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int age;
+	printf("enter your age : ");
+	scanf("%d",&age);
+	
+	if (age>=18){
+		printf("you are eligible to drive.\n");
+	
+	}
+	else{
+		printf("You are not eligible to drive.\n");
+	}
+}
+
